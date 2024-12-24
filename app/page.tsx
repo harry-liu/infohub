61c3fec43a40e50de952a0b8a9eb3c6ff9b431ab
Harry Liu <747311978@qq.com>
2024-12-24 11:36:18 +0800

    update style

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ async function TrendingProjects() {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="p-6">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-3xl font-bold">GitHub Trending Projects</h1>
         <Suspense fallback={<div>Loading trending projects...</div>}>
